b1cd7de0a82f556a0a1b2a3163c4f44d789fed48
BenjaminWhittaker <benjaminalwhittaker@gmail.com>
2025-08-16 17:23:09 +0200

    Testing pyo3 inheritance

--- python_script_test/init.py
+++ python_script_test/init.py
@@ -1,5 +1,15 @@
 from vetche_api import *
 
 def start():
-    obj = Object()
+
+    vec2 = Vec2(10, 10)
+
+    obj = Object("Test")
     obj.print()
+
+    node = Node("Child", vec2)
+    node.print()
+
+    node.position.x = 11
+
+    print(node.name, node.position.x)
--- vetche_api/src/lib.rs
+++ vetche_api/src/lib.rs
@@ -1,25 +1,72 @@
 use pyo3::prelude::*;
 
 #[pyclass]
-pub struct Object {
+#[derive(Clone)]
+pub struct Vec2 {
+    #[pyo3(get, set)]
+    pub x: i32,
+    #[pyo3(get, set)]
+    pub y: i32,
+}
 
+#[pymethods]
+impl Vec2 {
+    #[new]
+    pub fn new(x: i32, y: i32) -> Self {
+        return Self { x, y };
+    }
+}
+
+#[pyclass(subclass)]
+pub struct Object {
+    #[pyo3(get)]
+    pub name: String,
 }
 
 #[pymethods]
 impl Object {
     #[new]
-    pub fn new() -> Self {
-        Self{}
+    pub fn new(name: String) -> Self {
+        return Self { name };
     }
 
     pub fn print(&self) {
-        println!("Hello, this is an object: {:p}", self);
+        println!("Hello, this is an object with name '{}'", self.name);
+    }
+}
+
+#[pyclass(extends=Object)]
+pub struct Node {
+    #[pyo3(get, set)]
+    pub position: Vec2,
+}
+
+#[pymethods]
+impl Node {
+    #[new]
+    pub fn new(name: String, position: Vec2) -> (Self, Object) {
+        let object = Object { name };
+        let node = Self { position: position };
+
+        return (node, object);
+    }
+
+    pub fn print<'py>(self_: PyRef<'py, Self>) {
+        let position = self_.position.clone();
+        let base = self_.into_super();
+
+        println!(
+            "Hello, this is an node with name '{}' and position ({},{})",
+            base.name, position.x, position.y
+        );
     }
 }
 
 /// This defines the actual python module to be imported.
 #[pymodule]
 pub fn vetche_api(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<Vec2>()?;
     m.add_class::<Object>()?;
+    m.add_class::<Node>()?;
     Ok(())
 }
