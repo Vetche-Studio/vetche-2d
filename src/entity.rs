3700be3f12a8cf820a1f9eab0369adf4f6b9efab
Alan Rostem <42648988+AlanRostem@users.noreply.github.com>
2025-08-21 22:41:26 +0200

    Hmm... Trying to find a good way to do it...

--- src/entity.rs
+++ src/entity.rs
@@ -5,32 +5,55 @@ pub struct Vec2<T> {
     pub y: T,
 }
 
+impl<T> Clone for Vec2<T> where T : Clone {
+    fn clone(&self) -> Self {
+        Self {
+            x: self.x.clone(),
+            y: self.y.clone(),
+        }
+    }
+}
+
 type Vec2f = Vec2<f32>;
 
 pub struct Entity {
     pub position: Vec2f,
-    pub velocity: Vec2f
+    pub velocity: Vec2f,
+}
+
+impl Clone for Entity {
+    fn clone(&self) -> Self {
+        Self {
+            position: self.position.clone(),
+            velocity: self.velocity.clone(),
+        }
+    }
 }
 
 impl Entity {
     pub fn new(x: f32, y: f32) -> Self {
         Self {
-            position: Vec2f{x: x, y: y},
-            velocity: Vec2f{x: 0.0, y: 0.0},
+            position: Vec2f { x: x, y: y },
+            velocity: Vec2f { x: 0.0, y: 0.0 },
         }
+    }
+
+    pub fn set_velocity_x(&mut self, x: f32) {
+        self.velocity.x = x;
     }
 
     pub fn update(&mut self, delta: f32) {
         self.position.x += self.velocity.x * delta;
-        self.position.y += self.velocity.y * delta;   
+        self.position.y += self.velocity.y * delta;
     }
 
-    pub fn draw(&self, d: &mut RaylibDrawHandle<'_>) {
+    pub fn draw(&mut self, d: &mut RaylibDrawHandle<'_>) {
+        const SIZE: i32 = 32;
         d.draw_rectangle(
-            self.position.x as i32, 
-            self.position.y as i32, 
-            32, 
-            32, 
+            self.position.x as i32 - SIZE / 2,
+            self.position.y as i32 - SIZE / 2,
+            SIZE,
+            SIZE,
             color::Color::BLUE,
         );
     }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,68 @@
 use raylib::prelude::*;
 use rhai;
+use std::env;
+use std::fs;
+
+use crate::entity::Entity;
 
 mod entity;
 
+type EntityUID = usize;
 
-fn set_vel_x(x: f32) {
+#[derive(Clone)]
+struct Engine {
+    entities: Vec<entity::Entity>,
+}
 
+impl Engine {
+    fn new() -> Self {
+        Self {
+            entities: Vec::new(),
+        }
+    }
+
+    fn instantiate_entity(&mut self) -> EntityUID {
+        self.entities.push(Entity::new(0.0, 0.0).clone());
+        return self.entities.len() - 1;
+    }
+
+    fn start(&mut self) {
+        let (mut rl, thread) = raylib::init().size(640, 480).title("Hello, World").build();
+
+        rl.set_target_fps(60);
+        const DELTA: f32 = 1.0 / 60.0;
+        while !rl.window_should_close() {
+            for entity in &mut self.entities {
+                entity.update(DELTA);
+            }
+
+            let mut d: RaylibDrawHandle<'_> = rl.begin_drawing(&thread);
+            d.clear_background(Color::RAYWHITE);
+            for entity in &mut self.entities {
+                entity.draw(&mut d);
+            }
+        }
+    }
 }
 
 fn main() {
     let mut engine = rhai::Engine::new();
+    engine
+        .register_type_with_name::<Engine>("vetche")
+        .register_fn("start", Engine::start)
+        .register_fn("new_entity", Engine::instantiate_entity);
+    let mut scope = rhai::Scope::new();
+    scope.push("vetche", Engine::new());
 
-    engine.register_fn("set_vel_x",set_vel_x);
-    let mut e = entity::Entity::new(100.0, 200.0);
+    let file_path = "./test_scripts/entity.rhai";
 
-    let (mut rl, thread) = raylib::init().size(640, 480).title("Hello, World").build();
+    println!("Reading .rhai script '{file_path}'");
 
-    rl.set_target_fps(60);
+    let script_contents =
+        fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
-        d.clear_background(Color::RAYWHITE);
-        e.update(1.0 / 60.0);
-        e.draw(&mut d);
+    match engine.run_with_scope(&mut scope, script_contents.as_str()) {
+        Ok(_) => println!("===== Success ====="),
+        Err(e) => println!("{}", e),
     }
 }
